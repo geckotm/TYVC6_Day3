9a4a5a89d868b857d5d86e9e6cba7612c8d27656
geckotm <geckotm@gmail.com>
2015-09-11 14:51:42 +0300

    Draw a pixel

---
+++ Mouse/MouseDlg.h
@@ -0,0 +1,37 @@
+
+// MouseDlg.h : header file
+//
+
+#pragma once
+
+
+// CMouseDlg dialog
+class CMouseDlg : public CDialogEx
+{
+// Construction
+public:
+	CMouseDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_MOUSE_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+private:
+	int PrevX;
+	int PrevY;
+};
